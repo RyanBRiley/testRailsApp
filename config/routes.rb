333d0499e54f5a37a48dfa3326005912bb68835c
Ryan Riley <Ryan.B.Riley@colorado.edu>
2013-10-14 10:07:37 -0600

    Updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 TestApp::Application.routes.draw do
   root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
   resources :microposts
-
+  resources :sessions, only: [:new, :create, :destroy]
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
